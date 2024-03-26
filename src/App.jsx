ec591cabf4cf86003e3f58dc0a5fec8d8349c75e
zhangting <1092923594@qq.com>
2024-03-26 18:49:29 +0800

    update: useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 const { createRoot } = ReactDOM;
+const { useState } = React;
 /**
  * data => state
  * 
  * React => view library
  * 
+ * useState 创建一个状态与设置状态的方法。
  */
 const root = createRoot(document.getElementById('app'));
 
 function App () {
+
+    let [count, setCount] = useState(0);
+
+    function updateCount() {
+        setCount(count + 1)
+    }
+
     return (
-        <div>Hello React!!!!</div>
+        <div>
+            <div>{ count }</div>
+            <button onClick={updateCount}>count+1</button>
+        </div>
     )
 }
 
