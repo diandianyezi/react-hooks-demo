91ba1bccd55dac8d3acef3d6b52599920cdf53e8
zhangting <1092923594@qq.com>
2024-04-02 21:55:34 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,52 @@ const { useState } = React;
  */
 const root = createRoot(document.getElementById('app'));
 
-function App () {
+// function App () {
 
-    let [count, setCount] = useState(0);
+//     let [count, setCount] = useState(0);
 
-    function updateCount() {
-        setCount(count + 1)
+//     function updateCount() {
+//         setCount(count + 1)
+//     }
+
+//     return (
+//         <div>
+//             <div>{  count }</div>
+//             <button onClick={updateCount}>count+1</button>
+//         </div>
+//     )
+// }
+
+class Person extends React.Component {
+    render() {
+        const { name, age, sex} = this.props
+        return (
+            <ul>
+                <li>{name}</li>
+                <li>{age}</li>
+                <li>{sex}</li>
+            </ul>
+        )
     }
-
-    return (
-        <div>
-            <div>{ count }</div>
-            <button onClick={updateCount}>count+1</button>
-        </div>
-    )
+    // 需要引入prop-types.js
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+        sex: PropTypes.string,
+        age: PropTypes.number,
+        speak: PropTypes.func
+    }
+    static defaultProps = {
+        sex: '女',
+        age: 19
+    }
 }
 
-root.render(<App />)
+const p = {
+    name: 'r',
+    age: 12,
+    sex: '男'
+}
+
+root.render(<Person {...p} />)
+
+//   
