d53e463d05efba2b6a134cd68f1d0b201a6f28f4
zhangting <1092923594@qq.com>
2024-04-02 22:30:32 +0800

    update

--- src/life2.jsx
+++ src/life2.jsx
@@ -17,16 +17,29 @@ class App extends Component {
             count: count + 1
         })
     }
-    componentDidMount() {
 
+    remove = () => {
+        root.unmount();
+    }
+    componentDidMount() {
+        console.info('componentDidMount')
     }
 
     componentDidUpdate() {
-
+        console.info('componentDidUpdate')
     }
 
     componentWillUnmount() {
+        console.info('componentWillUnmount')
+    }
 
+    shouldComponentUpdate() {
+        console.info('shouldComponentUpdate');
+        return true
+    }
+
+    force = () => {
+        this.forceUpdate();
     }
     
     render() {
@@ -36,9 +49,56 @@ class App extends Component {
             <div>
                 <h2>当前求和为{count}</h2>
                 <button onClick={this.add}>点我+1</button>
+                <button onClick={this.remove}>卸载</button>
+                <button onClick={this.force}>强制更新</button>
             </div>
         )
     }
 }
 
-root.render(<App />)
+class A extends Component {
+    state = {
+        carName: '奔驰'
+    }
+
+    changeCar = () => {
+        this.setState({
+            carName: '宝马'
+        })
+    }
+    render() {
+        return (
+            <div>
+                <div>我是A组件</div>
+                <button onClick={this.changeCar}>换车</button>
+                <B carName={this.state.carName}>
+                    
+                </B>
+            </div>
+        )
+    }
+}
+
+class B extends Component {
+    componentDidMount() {
+        console.info('子组件----componentDidMount')
+    }
+    getSnapshotBeforeUpdate(props) {
+        // 首次渲染不会执行？
+        console.info('子组件----getSnapshotBeforeUpdate', props)
+    }
+    shouldComponentUpdate() {
+        console.info('子组件----shouldComponentUpdate');
+        return true
+    }
+    render() {
+        console.info('组组件----render')
+        return (
+            <div>
+                我是B组件
+            {this.props.carName}
+            </div>
+        )
+    }
+}
+root.render(<A />)
